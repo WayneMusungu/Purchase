bd80e794dd63651ea7b79875a3bb552cb1a94d73
WayneMusungu <waynemuyera17@gmail.com>
2023-09-25 16:47:50 +0300

    Update Database

--- purchase/settings.py
+++ purchase/settings.py
@@ -86,12 +86,24 @@ WSGI_APPLICATION = 'purchase.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config("DATABASE_NAME"),
+        'USER': config("DATABASE_USER"),
+        'PASSWORD': config("DATABASE_PASSWORD"),
+        'HOST': config("DATABASE_HOST"),
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
