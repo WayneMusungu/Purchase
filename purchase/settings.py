bd0dc6d48974f272d5527ce7f747409d307ed2e0
WayneMusungu <waynemuyera17@gmail.com>
2023-09-25 17:04:15 +0300

    Update variables

--- purchase/settings.py
+++ purchase/settings.py
@@ -25,8 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
-API_KEY = config('api_key')
-USERNAME = config('username')
+API_KEY = config('API_KEY')
+USERNAME = config('USERNAME')
 
 
 
