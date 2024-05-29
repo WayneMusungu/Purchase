0a1bd728f5b55ded1ce4db9b160d499df57cdd70
wayne <waynemuyera17@gmail.com>
2024-05-29 16:14:34 +0300

    Make Changes

--- purchase/settings.py
+++ purchase/settings.py
@@ -29,8 +29,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 API_KEY = config('API_KEY')
-username = config('username')
-sender = config('sender') 
+USERNAME = config('USERNAME')
+SENDER = config('SENDER') 
 
 
 
